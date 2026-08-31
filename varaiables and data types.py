cde37a3bfa51c8bc1b6ce586102a40929fa0d85f
darshan-devloper123 <darshandarshi98765@gmail.com>
2026-08-31 21:33:17 +0530

    Add files via upload

---
+++ varaiables and data types.py
@@ -0,0 +1,19 @@
+#Data types.. 
+name ="darshan"  #< class string>
+age = 22   #<class int>
+height = 6.1  #<class flat>
+print(type(name))
+print(type(age))
+print(type(height))
+
+
+print(f"Name: {name}, Age: {age}, Height: {height} ")
+
+
+age = 21
+is_degree_student = True  #<class bool>
+print(type(is_degree_student))
+print(is_degree_student)
+
+
+
