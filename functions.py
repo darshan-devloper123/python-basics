46536acf807a4fd5d9238c6c167c357f41d70dfc
darshan-devloper123 <darshandarshi98765@gmail.com>
2026-09-01 15:59:12 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,67 @@
+print("Example fuctions..")
+def greet():
+    print("hello")
+
+greet()
+
+
+print("Function with parameters:")
+def dating(men,women):
+    print(f"Men: is {men}")
+    print(f"Wommen: is {women}")
+    print(f"{men} Dating {women}")
+
+dating("virat","Anushka")
+
+print("Returning values:")
+def add(a,b):
+    return a + b
+
+result = add(10,20)
+print("The sum is: ",result)
+
+
+        
+print("defult paramter values:")
+def Introduce(name,age=21):
+    print("I am ",name)
+    print("My age is",age)
+
+Introduce("darshan")
+
+
+def student(name,course,marks):
+    print(f"Name: {name}, Course: {course}, Marks: {marks}")
+
+student("Darshan", "BCA",99)
+
+
+print("Function with parameters:")
+def dating(men,women):
+    print(f"Men: is {men}")
+    print(f"Wommen: is {women}")
+    print(f"{men} Dating {women}")
+
+dating("virat","Anushka")
+
+print("Returning values:")
+def add(a,b):
+    return a + b
+
+result = add(10,20)
+print("The sum is: ",result)
+
+
+        
+print("defult paramter values:")
+def Introduce(name,age=21):
+    print("I am ",name)
+    print("My age is",age)
+
+Introduce("darshan")
+
+
+def student(name,course,marks):
+    print(f"Name: {name}, Course: {course}, Marks: {marks}")
+
+student("Darshan", "BCA",99)
